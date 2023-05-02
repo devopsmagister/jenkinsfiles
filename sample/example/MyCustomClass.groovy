732e9f9f79f4f1b392b18e7c60b47994ecd99432
Chiranjit Ghosh <chiranjitghosh85@gmail.com>
2023-05-02 18:13:52 +0530

    added files

---
+++ sample/example/MyCustomClass.groovy
@@ -0,0 +1,16 @@
+package sample.example
+class MyCustomClass {
+    String name
+    String test
+    
+    MyCustomClass(String name, String test) {
+        this.name = name
+        this.test = test
+    }
+    
+    void greet() {
+        println("Hello, ${this.name} ${this.test}!")
+        def myEnvVar = System.getenv("JAVA_HOME")
+        println "The value of MY_ENV_VAR is: ${myEnvVar}"
+    }
+}
---
+++ script.groovy
@@ -0,0 +1,7 @@
+// Import the MyCustomClass class from the file
+// package com.example
+import sample.example.MyCustomClass
+
+// Create an instance of the class and call its methods
+def myInstance = new MyCustomClass("Chiranjit", "Ghosh")
+myInstance.greet()
